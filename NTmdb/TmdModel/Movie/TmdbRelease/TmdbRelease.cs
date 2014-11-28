908511571ba0866436ff87d0ac74facf4ac5b6c4
Patrick Kalkman <pkalkie@gmail.com>
2014-11-28 19:18:58 +0100

    Renamed Release folder to TmdbRelease so that it won't get ignored

---
+++ NTmdb/TmdModel/Movie/TmdbRelease/TmdbRelease.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NTmdb
+{
+    /// <summary>
+    ///     Class representing the TMDb release object.
+    /// </summary>
+    /// <remarks>
+    ///     Details: http://docs.themoviedb.apiary.io/#movies
+    /// </remarks>
+    public class TmdbRelease : TmdbModelBase
+    {
+        /// <summary>
+        ///     Gets or sets the ISO 3166-1 country code of the country where the release comes from.
+        /// </summary>
+        /// <value>The ISO 3166-1 country code of the country where the release comes from.</value>
+        [JsonProperty( PropertyName = "iso_3166_1" )]
+        public String Iso3166_1 { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the certification of the release.
+        /// </summary>
+        /// <value>The certification of the release.</value>
+        [JsonProperty( PropertyName = "certification" )]
+        public String Certification { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the release data.
+        /// </summary>
+        /// <value>The release data.</value>
+        [JsonProperty( PropertyName = "release_date" )]
+        public String ReleaseDate { get; set; }
+    }
+}
---
+++ NTmdb/TmdModel/Movie/TmdbRelease/TmdbReleases.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NTmdb
+{
+    /// <summary>
+    ///     Class representing the TMDb releases object.
+    /// </summary>
+    /// <remarks>
+    ///     Details: http://docs.themoviedb.apiary.io/#movies
+    /// </remarks>
+    public class TmdbReleases : TmdbModelBase
+    {
+        /// <summary>
+        ///     Gets or sets the ID of the movie to which the releases belongs.
+        /// </summary>
+        /// <value>The ID of the movie to which the releases belongs.</value>
+        [JsonProperty( PropertyName = "id" )]
+        public Int32 Id { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the releases.
+        /// </summary>
+        /// <value>The  releases.</value>
+        [JsonProperty( PropertyName = "countries" )]
+        public List<TmdbRelease> Releases { get; set; }
+    }
+}
